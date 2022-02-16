6e2cfcf276dd38dd9f8c5d16e7805320c1d3eea3
AstralShiro <greenshadow0098@gmail.com>
2022-02-16 21:52:28 +0100

    Weapons

---
+++ Items/Weapons/Sword/Grandblue.cs
@@ -0,0 +1,41 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Elementaria.Items.Weapons.Sword
+{
+	public class Grandblue : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			// DisplayName.SetDefault("Test"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("Apply chilled on the hit");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 50;
+			item.melee = true;
+			item.width = 50;
+			item.height = 50;
+			item.useTime = 35;
+			item.useAnimation = 35;
+			item.useStyle = 1;
+			item.knockBack = 6;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+
+			item.buffType = BuffID.Chilled;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ModContent.ItemType<Items.Materials.IceStone>(), 2);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
