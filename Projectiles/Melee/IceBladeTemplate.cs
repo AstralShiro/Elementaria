1e0450f845face5b2d51249b1be9e4e8b7ff4b91
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-03-17 15:01:37 -0400

    Add files via upload

---
+++ Projectiles/Melee/IceBladeTemplate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Elementaria.Projectiles.Melee
+{
+    public class IceBladeTemplate : ModProjectile //You will need to add sprites for them
+    {
+        public override void SetDefaults()
+        {
+            projectile.ignoreWater = false;
+            projectile.width = 24;
+            projectile.penetrate = 1;
+            projectile.height = 24;
+            projectile.friendly = true;
+            projectile.light = 1f;
+            projectile.tileCollide = true;
+            projectile.aiStyle = 28;
+        }
+
+        public override void AI()
+        {
+            if (Main.rand.NextBool(6))
+            {
+                Dust.NewDust(projectile.Center, projectile.width, projectile.height, DustID.Snow); //Add any dust here
+            }
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Main.PlaySound(SoundID.Dig, projectile.position);
+            return false;
+        }
+    }
+}
