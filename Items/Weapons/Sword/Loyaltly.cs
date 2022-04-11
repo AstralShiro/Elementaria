1dac3cd6ac6725e8726e5db1972e8665258bb967
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-04-11 13:11:35 -0400

    Spotlight was uh.....to bright

--- Items/Weapons/Sword/Loyaltly.cs
+++ Items/Weapons/Sword/Loyaltly.cs
@@ -39,32 +39,5 @@ namespace Elementaria.Items.Weapons.Sword
             Projectile.NewProjectile(position, new Vector2(speedX, speedY), type, damage, knockBack, player.whoAmI, 0, dir);
             return false;
         }
-
-        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
-        {
-            Vector2 pos = item.Center -  Main.screenPosition;
-            Texture2D texture = Main.itemTexture[item.type];
-            Texture2D spot = ModContent.GetTexture("Elementaria/Textures/Spotlight");
-            float alpha = 0.8f;
-            float s = 0.5f * scale;
-
-            for (int i = 0; i < 6; i++)
-            {
-                spriteBatch.Draw(
-                    spot,
-                    pos,
-                    null,
-                    Color.Lerp(Color.White, Color.LightYellow, (float)i / 3) * alpha,
-                    0f,
-                    new Vector2(spot.Width, spot.Height) / 2,
-                    s * (1f + ((float)Math.Sin((Main.GlobalTime * 2) + (MathHelper.PiOver4 * i)) * 0.15f)),
-                    SpriteEffects.None,
-                    0f);
-                s *= 2;
-                alpha *= 0.9f;
-            }
-            spriteBatch.Draw(texture, pos, null, Color.White, -MathHelper.PiOver4, new Vector2(texture.Width, texture.Height) / 2, scale, SpriteEffects.None, 0f);
-            return false;
-        }
     }
 }
